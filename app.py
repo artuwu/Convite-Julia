9ca69049803b7d9036dd1502b9989613f0a35803
Artur Mendes <arturm.israel@gmail.com>
2026-02-19 13:23:43 -0300

    main

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ cadastrar_convidados_iniciais()
 # ==============================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
